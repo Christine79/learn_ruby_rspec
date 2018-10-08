1ef17ee9f2eb92ee405ec28e54c4472bd31b22dd
Christine79 <christinejaouen@hotmail.com>
2018-10-08 20:54:59 +0200

    2ème commit

--- 04_simon_says/simon_says.rb
+++ 04_simon_says/simon_says.rb
@@ -15,3 +15,39 @@ def start_of_word(a,b)
     n=b.to_i
     a.byteslice(0..(n-1))
 end
+def first_word(a)
+    a.partition(" ").first
+end
+def titleize(a)
+#   puts a
+   puncs = ["and", "the", "to", "of", "by", "from", "or"]
+   a.split(" ").map {|word| word.capitalize}.join(" ")
+   a.split.map(&:capitalize)*' '
+#   if a.puncs.include?
+#      a.downcase
+#    else
+#      a.capitalize
+#   end
+#
+#   a.partition(" ").first.capitalize
+end
+#def titleize(a)
+#puncs = ["and", "the", "to", "of", "by", "from", "or"]
+#        array = a.split(" ")
+#        array = a
+#if a.split(' ').size > 1
+#puts array
+#        array.map! do |x|
+#            if puncs.include?
+#              if x.to_i = 0
+#                 x.capitalise
+#              else
+#                x.downcase
+#              end
+#            else
+#                w.capitalize
+#            end
+#         end
+#       end
+#        return array.join(" ")
+#    end
