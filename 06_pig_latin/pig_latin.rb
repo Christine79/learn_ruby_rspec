764050337fe909e27568a81d64745e9750eef26b
Christine79 <christinejaouen@hotmail.com>
2018-10-08 23:51:51 +0200

    6ème commit

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,20 +1,34 @@
 def translate(n)
-
+    puts n
     if n[0].count("aeiou") > 0
        str = n
        str << "ay"
     else
     str = "aeiou"
     p = 0
-    if str.include?  n[0]
-       p = p
-    else
+
+
+    if (str.include? n[0]) == false &&  (str.include? n[1]) == false
+      m = n.length
+      q=n[0..1]
+      o = m-1
+      str = n[2..o]
+      str << q
+      str << "ay"
+      return str
+    end
+
+    if (str.include? n[0]) == false
       m = n.length
       q=n[0]
       o = m-1
       str = n[1..o]
       str << q
       str << "ay"
+      return str
     end
+
+
+
   end
 end
